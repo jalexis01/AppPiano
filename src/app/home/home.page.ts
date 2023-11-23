d82245ffaf351082577b9bb8ef017231bf6de660
jalexisreyes <jalexisreyes@estudiante.uniajc.edu.co>
2023-11-23 11:57:57 -0500

    Commit Inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  listElements : any[]=[
+    {numeroSonido: 1, color:'#0d6efd'},
+    {numeroSonido: 2, color:'#6c757d'},
+    {numeroSonido: 3, color:'#198754'},
+    {numeroSonido: 4, color:'#dc3545'},
+    {numeroSonido: 5, color:'#ffc720'},
+    {numeroSonido: 6, color:'#25cff2'},
+    {numeroSonido: 7, color:'#373b3e'},
+  ]
+
   constructor() {}
+
+  aplicarSonido(numeroSonido: number){
+    const audio = new Audio();
+
+    audio.src ='../../assets/sonidos/note'+numeroSonido+'.wav';
+    audio.load();
+    audio.play();
+  }
 
 }
